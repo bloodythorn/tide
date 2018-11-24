9b52d8bffbbe05a6ff812cd18e12873d733f4ffd
bloodythorn <bloodythorn@protonmail.com>
2018-11-24 06:17:33 -0700

    Added cmake Modules

--- examples/console/console.cpp
+++ examples/console/console.cpp
@@ -1,7 +1,39 @@
+#include "examples/console/console.hpp"
+
+#include "examples/console/state_console.hpp"
+
+#include <tide/engine/logger.hpp>
+
+#include <memory>
+
+Console::Console(int, char**) { }
+
+void Console::Start(void) {
+  m_sm.addStartState(
+    tide::Engine::StateManager::StatePtr(new State_Console()));
+  start();
+}
+
+/** Main Entry Point **/
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 int main(int argc, char** argv) {
-  std::cout << "Hello World" << std::endl;
+  static const std::wstring MODULE_NAME{L"Main"};
+  LOG_INFO(MODULE_NAME) << __func__ << "()-Initializing Logger.";
+  LOG_STAT.setReportLevel(MODULE_NAME, tide::Engine::Log::Debug);
+
+  LOG_INFO(MODULE_NAME) << __func__ << "()-Starting Engine.";
+  try { Console c(argc, argv); c.Start(); }
+  catch(const std::exception& e) {
+    LOG_ERROR(MODULE_NAME) << __func__ << ":Exception:" << e.what();
+    return EXIT_FAILURE;
+  }
+  catch(...) {
+    LOG_ERROR(MODULE_NAME) << __func__ << ":Unknown Exception";
+    return EXIT_FAILURE;
+  }
+
   return EXIT_SUCCESS;
 }
--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,1 +1,14 @@
 #include "tide/engine/engine.hpp"
+
+namespace tide {
+namespace Engine {
+
+/*TODO:FIXME1 */
+Engine::Engine(void) : Loop(60) { }
+
+void Engine::handle(double p_dt) { m_sm.handle(*this, p_dt); }
+void Engine::update(double p_dt) { m_sm.update(*this, p_dt); }
+void Engine::render(double p_dt) { m_sm.render(*this, p_dt); }
+
+}//Engine
+}//tide
