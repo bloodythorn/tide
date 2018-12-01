01e1a4ff9fd3ab61e51585aa511e8523942c26cf
bloodythorn <bloodythorn@protonmail.com>
2018-12-01 09:07:26 -0700

    ChessBoard working -> Still need to doc + cleanup

--- tests/test_chess.cpp
+++ tests/test_chess.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <cstdlib>
 
+#include <algorithm>
+#include <array>
+#include <deque>
+#include <exception>
+#include <random>
+
 #include <tide/games/chess/chess.hpp>
 
+using namespace tide::Games::Chess;
+using namespace std;
+
+bool ChessBoardTests(std::deque<std::wstring>& er) {
+  return false;
+}
+
 int main(int argc, char** argv) {
-  std::cout << "Hello World" << std::endl;
-  tide::Games::Chess::ChessBoard test;
+  std::wcout << "***Chess Tests***" << std::endl;
+  ChessBoard test;
+
+  std::wcout << L"ChessBoard Tests:";
+
+
+
   return EXIT_SUCCESS;
 }
