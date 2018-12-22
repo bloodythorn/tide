e36d46b8bee8ba970a55dbc0b3745db7e494cfb6
bloodythorn <bloodythorn@protonmail.com>
2018-12-21 21:27:12 -0700

    added default throws to class functions

--- snippets/class.cpp
+++ snippets/class.cpp
@@ -1,19 +1,27 @@
 #include "class.hpp"
 
-Class::Class() { }
+#include <stdexcept>
 
-Class::Class(const Class& p_ot) { }
+Class::Class() { throw new std::runtime_error("Unimplemented Constructor"); }
 
-Class::Class(Class&& p_ot) { }
+Class::Class(const Class& p_ot) {
+  throw new std::runtime_error("Unimplemented Copy Constructor");
+}
 
-Class::~Class() { }
+Class::Class(Class&& p_ot) {
+  throw new std::runtime_error("Unimplemented move Constructor");
+}
+
+Class::~Class() { throw new std::runtime_error("Unimplemented Destructor"); }
 
 Class& Class::operator=(const Class& p_ot) {
+  throw new std::runtime_error("Unimplemented Copy Assignment");
   if(this != &p_ot) {};
   return *this;
 }
 
 Class& Class::operator=(Class&& p_ot) {
+  throw new std::runtime_error("Unimplemented Move Assignment");
   if(this != &p_ot) {};
   return *this;
 }
