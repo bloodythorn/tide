f6b108bddfc00f49ce626ea47d289f667a302bb7
bloodythorn <thorn.mg@gmail.com>
2018-12-23 01:12:37 -0700

    updates to various things

--- include/examples/console/state_console.hpp
+++ include/examples/console/state_console.hpp
@@ -1,11 +1,14 @@
 #ifndef STATE_CONSOLE_HPP
 #define STATE_CONSOLE_HPP
 
+#include <tide/engine/engine.hpp>
 #include <tide/engine/state.hpp>
 
 class State_Console : public tide::Engine::State {
 public:
   const std::wstring Name(void) const override;
+  void update(tide::Engine::Engine& p_en, double p_dt) override;
+  void render(tide::Engine::Engine& p_en, double p_dt) override;
 protected:
 private:
 };
--- include/tide/engine/engine.hpp
+++ include/tide/engine/engine.hpp
@@ -1,21 +1,25 @@
 #ifndef ENGINE_HPP
 #define ENGINE_HPP
 
+#include <memory> //unique_ptr
+
 #include "tide/engine/logger.hpp"
 #include "tide/engine/loop.hpp"
 #include "tide/engine/noncopyable.hpp"
+#include "tide/engine/platform.hpp"
 #include "tide/engine/state.hpp"
 #include "tide/engine/statemanager.hpp"
 
-namespace tide {
-namespace Engine {
+namespace tide { namespace Engine {
 
 class Engine : protected Loop, private NonCopyable {
 public:
   Engine(void);
+  virtual void Start(void) = 0;
 protected:
 
   StateManager m_sm;
+  std::unique_ptr<Platform::Platform> m_platform;
 
 private:
 
@@ -23,7 +27,6 @@ private:
   void update(double) override;
   void render(double) override;
 };
-}
-}
+}/*Engine*/}/*tide*/
 
 #endif //ENGINE_HPP
