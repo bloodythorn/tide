651feea0889c29ea1f1d52d3bddcc205394f6427
bloodythorn <bloodythorn@protonmail.com>
2019-04-30 19:35:11 -0600

    corrected class cpp

--- snippets/class.cpp
+++ snippets/class.cpp
@@ -2,26 +2,26 @@
 
 #include <stdexcept>
 
-Class::Class() { throw new std::runtime_error("Unimplemented Constructor"); }
+Class::Class() { throw std::runtime_error("Unimplemented Constructor"); }
 
 Class::Class(const Class& p_ot) {
-  throw new std::runtime_error("Unimplemented Copy Constructor");
+  throw std::runtime_error("Unimplemented Copy Constructor");
 }
 
 Class::Class(Class&& p_ot) {
-  throw new std::runtime_error("Unimplemented move Constructor");
+  throw std::runtime_error("Unimplemented move Constructor");
 }
 
-Class::~Class() { throw new std::runtime_error("Unimplemented Destructor"); }
+Class::~Class() { }
 
 Class& Class::operator=(const Class& p_ot) {
-  throw new std::runtime_error("Unimplemented Copy Assignment");
+  throw std::runtime_error("Unimplemented Copy Assignment");
   if(this != &p_ot) {};
   return *this;
 }
 
 Class& Class::operator=(Class&& p_ot) {
-  throw new std::runtime_error("Unimplemented Move Assignment");
+  throw std::runtime_error("Unimplemented Move Assignment");
   if(this != &p_ot) {};
   return *this;
 }
