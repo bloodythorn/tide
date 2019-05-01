8f172da73bee5bc84a267c6ff0e1b5261cf32010
bloodythorn <thorn.mg@gmail.com>
2019-04-30 23:32:53 -0600

    simple argument parser

---
+++ src/engine/options.cpp
@@ -0,0 +1,62 @@
+#include "tide/engine/options.hpp"
+
+#include <stdexcept>
+
+#include <deque>
+#include <vector>
+
+namespace tide { namespace Engine { namespace Options {
+
+ArgParser::ArgParser(int p_argc, char** p_argv) {
+
+  /* Put args in deque */
+  std::deque<std::string> exeArgs;
+  for(auto i = 0; i < p_argc; ++i) exeArgs.push_back(p_argv[i]);
+
+  /* Pull out executable name */
+  m_exeName = exeArgs.front(); exeArgs.pop_front();
+
+
+  /* Extract each option and it's args */
+  while(!exeArgs.empty()) {
+    /* Verify it's a valid command */
+    if(exeArgs.front()[0] == '-') {
+      Option option;
+      option.opt = exeArgs.front();
+      exeArgs.pop_front(); if(exeArgs.empty()) break;
+      while(exeArgs.front()[0] != '-') {
+        option.optArgs.push_back(exeArgs.front());
+        exeArgs.pop_front(); if(exeArgs.empty()) break;
+      }
+      this->push(option);
+    } else
+      throw
+        std::runtime_error(
+          "Error parsing command; wrong order in" + exeArgs.front()
+        );
+  }
+}
+
+ArgParser::ArgParser(const ArgParser& p_ot) {
+  throw std::runtime_error("Unimplemented Copy Constructor");
+}
+
+ArgParser::ArgParser(ArgParser&& p_ot) {
+  throw std::runtime_error("Unimplemented move Constructor");
+}
+
+ArgParser::~ArgParser() { }
+
+ArgParser& ArgParser::operator=(const ArgParser& p_ot) {
+  throw std::runtime_error("Unimplemented Copy Assignment");
+  if(this != &p_ot) {};
+  return *this;
+}
+
+ArgParser& ArgParser::operator=(ArgParser&& p_ot) {
+  throw std::runtime_error("Unimplemented Move Assignment");
+  if(this != &p_ot) {};
+  return *this;
+}
+
+}/*Options*/}/*Engine*/ }/*tide*/
