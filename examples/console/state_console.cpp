7e0aeadb7f2d1717f28955e973ca2557dd829813
bloodythorn <bloodythorn@protonmail.com>
2019-05-07 16:46:24 -0600

    Added handle routine

--- examples/console/state_console.cpp
+++ examples/console/state_console.cpp
@@ -4,6 +4,10 @@ const std::wstring State_Console::Name(void) const {
   return L"Console";
 }
 
+void State_Console::handle(tide::Engine::Engine& p_en, double p_dt) {
+
+}
+
 void State_Console::update(tide::Engine::Engine& p_en, double p_dt) {
 
 }
