9ef3ab21b226684b9a98a5cb336c01f6c7fa30c2
bloodythorn <bloodythorn@protonmail.com>
2019-05-07 17:16:32 -0600

    Added sysevent class

---
+++ include/tide/engine/sysevent.hpp
@@ -0,0 +1,20 @@
+#ifndef SYSEVENT_HPP
+#define SYSEVENT_HPP
+
+#include <memory>
+#include <ostream>
+
+namespace tide { namespace Engine { namespace Platform {
+
+class SysEvent {
+public:
+
+private:
+
+};
+
+using SysEventPtr = std::shared_ptr<SysEvent>;
+
+}/*Platform*/}/*Engine*/}/*tide*/
+
+#endif // SYSEVENT_HPP
