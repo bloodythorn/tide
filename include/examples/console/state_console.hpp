0acd3cc7eacf44ecfb0dd12217821b654cdad3e2
bloodythorn <bloodythorn@protonmail.com>
2019-05-07 21:25:37 -0600

    added handle back

--- include/examples/console/state_console.hpp
+++ include/examples/console/state_console.hpp
@@ -7,6 +7,7 @@
 class State_Console : public tide::Engine::State {
 public:
   const std::wstring Name(void) const override;
+  void handle(tide::Engine::Engine& p_en, double p_dt) override;
   void update(tide::Engine::Engine& p_en, double p_dt) override;
   void render(tide::Engine::Engine& p_en, double p_dt) override;
 protected:
--- include/tide/engine/state.hpp
+++ include/tide/engine/state.hpp
@@ -11,6 +11,7 @@ class Engine;
 class State {
 public:
   virtual const std::wstring Name(void) const = 0;
+  virtual void handle(Engine& p_en, double p_dt) = 0;
   virtual void update(Engine& p_en, double p_dt) = 0;
   virtual void render(Engine& p_en, double p_dt) = 0;
 protected:
